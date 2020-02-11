8f313a4a410f430f0eb8e0befcfb5cacac84ef45
reyhan37 <reyhan.hoq@gmail.com>
2020-02-10 20:58:00 -0500

    first commit

---
+++ my-module.js
@@ -0,0 +1,14 @@
+quotes = [
+    "Indeed, Allah will not change the condition of a people until they change what is in themselves. [13:11]",
+    "And do not walk upon the earth exultantly. Indeed, you will never tear the earth [apart], and you will never reach the mountains in height. [17:37]",
+    "And your Lord has decreed that you not worship except Him, and to parents, good treatment. Whether one or both of them reach old age [while] with you, say not to them [so much as], 'uff', and do not repel them but speak to them a noble word. And lower to them the wing of humility out of mercy and say, 'My Lord, have mercy upon them as they brought me up when I was small. [17:23-4]",
+    "O you who have believed, be persistently standing firm for Allah , witnesses in justice, and do not let the hatred of a people prevent you from being just. Be just; that is nearer to righteousness. And fear Allah ; indeed, Allah is Acquainted with what you do. [5:8]",
+    "Your god is one God. But those who do not believe in the Hereafter - their hearts are disapproving, and they are arrogant. Assuredly, Allah knows what they conceal and what they declare. Indeed, He does not like the arrogant. And when it is said to them, 'What has your Lord sent down?' They say, 'Legends of the former peoples,' That they may bear their own burdens in full on the Day of Resurrection and some of the burdens of those whom they misguide without knowledge. Unquestionably, evil is that which they bear. [16:22-5]",
+    "That is Jesus, the son of Mary - the word of truth about which they are in dispute. It is not befitting for Allah to take a son; exalted is He! When He decrees an affair, He only says to it, 'Be,' and it is. (Jesus said), 'And indeed, Allah is my Lord and your Lord, so worship Him. That is a straight path.' [19:34-6]"
+];
+
+var randomVerse = quotes[Math.floor(Math.random()*quotes.length)];
+
+module.exports = {
+    randomVerse
+}
